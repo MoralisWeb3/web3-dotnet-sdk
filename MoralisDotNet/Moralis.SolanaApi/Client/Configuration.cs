9db75efc96e9b5f31e62f188a3ffa4ba24786f6a
xactant <xactant@gmail.com>
2022-06-28 14:15:09 -0400

    NuGet package updates

--- MoralisDotNet/Moralis.SolanaApi/Client/Configuration.cs
+++ MoralisDotNet/Moralis.SolanaApi/Client/Configuration.cs
@@ -16,7 +16,7 @@ namespace Moralis.SolanaApi.Client
         /// Version of the package.
         /// </summary>
         /// <value>Version of the package.</value>
-        public const string Version = "1.0.0";
+        public const string Version = "1.0.2";
   
         /// <summary>
         /// Gets or sets the default API client for making HTTP calls.
@@ -123,7 +123,7 @@ namespace Moralis.SolanaApi.Client
                      .GetReferencedAssemblies()
                      .Where(x => x.Name == "System.Core").First().Version.ToString()  + "\n";
             report += "    Version of the API: 2\n";
-            report += "    SDK Package Version: 1.0.0\n";
+            report += "    SDK Package Version: 1.0.2\n";
   
             return report;
         }
